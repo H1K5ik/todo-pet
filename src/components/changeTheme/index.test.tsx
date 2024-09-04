90a5c60a9b90ce7a69a3a772e148179fbfe15c7f
H1K5ik <nikikra2323@gmail.com>
2024-09-04 03:58:50 +0300

    test: add to changeTheme

---
+++ src/components/changeTheme/index.test.tsx
@@ -0,0 +1,62 @@
+import { ThemeProvider } from '@component/themeContext'
+import { render } from '@testing-library/react'
+import React from 'react'
+import '@testing-library/jest-dom'
+import { useMedia } from 'react-media-hook'
+
+import { ChangeTheme } from './index'
+
+jest.mock('react-media-hook', () => ({
+  useMedia: jest.fn(),
+}))
+describe('Test changeTheme', () => {
+  test('Should render the theme switcher correctly on desktop', () => {
+    const { getByText } = render(
+      <ThemeProvider>
+        <ChangeTheme />
+      </ThemeProvider>,
+    )
+
+    const switchThemeText = getByText('Switch Theme')
+    expect(switchThemeText).toBeInTheDocument()
+
+    const lightThemeOption = getByText('Light theme')
+    expect(lightThemeOption).toBeInTheDocument()
+
+    const darkThemeOption = getByText('Dark theme')
+    expect(darkThemeOption).toBeInTheDocument()
+  })
+
+  test('Should render the theme switcher with the correct options', () => {
+    const { getByText } = render(
+      <ThemeProvider>
+        <ChangeTheme />
+      </ThemeProvider>,
+    )
+
+    const switchThemeText = getByText('Switch Theme')
+    expect(switchThemeText).toBeInTheDocument()
+
+    const lightThemeOption = getByText('Light theme')
+    expect(lightThemeOption).toBeInTheDocument()
+
+    const darkThemeOption = getByText('Dark theme')
+    expect(darkThemeOption).toBeInTheDocument()
+  })
+
+  jest.mock('react-media-hook')
+
+  test('should render the theme switcher correctly on mobile', () => {
+    ;(useMedia as jest.Mock).mockReturnValueOnce({ matches: true })
+
+    const { getByText } = render(
+      <ThemeProvider>
+        <ChangeTheme />
+      </ThemeProvider>,
+    )
+
+    expect(getByText('Switch Theme')).toBeInTheDocument()
+    expect(getByText('Light theme')).toBeInTheDocument()
+    expect(getByText('Dark theme')).toBeInTheDocument()
+  })
+})
