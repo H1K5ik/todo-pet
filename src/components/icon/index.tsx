b37dddc48c68da86b579c15c3f8bff278b2d267e
H1K5ik <nikikra2323@gmail.com>
2024-09-05 12:10:50 +0300

    refactor: update styles on icons

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,17 +1,15 @@
-import { useTheme } from '@component/themeContext'
 import React, { FC } from 'react'
 import { useMedia } from 'react-media-hook'
 
 import { IItems } from './interfaces'
-import { WrapperIcons } from './styles'
+import { Icon, WrapperIcons } from './styles'
 
 export const Icons: FC<IItems> = ({ alt, onClick, src }) => {
-  const { isLight } = useTheme()
   const isMobile = useMedia('(max-width: 600px)')?.matches
 
   return (
-    <WrapperIcons $isLight={isLight}>
-      <img alt={alt} onClick={onClick} src={src} width={`${isMobile ? '45px' : '50px'}`} />
+    <WrapperIcons>
+      <Icon $isMobile={isMobile ?? false} alt={alt} onClick={onClick} src={src} />
     </WrapperIcons>
   )
 }
--- src/components/icon/styles.ts
+++ src/components/icon/styles.ts
@@ -1,12 +1,13 @@
-import { colors } from '@theme'
 import { styled } from 'styled-components'
 
-export const WrapperIcons = styled.div<{ $isLight: boolean }>`
+export const WrapperIcons = styled.div`
   margin: 5px;
-  transition: box-shadow 0.2s ease-in;
+`
+export const Icon = styled.img<{ $isMobile: boolean }>`
+  width: ${(props: { $isMobile: boolean }) => (props.$isMobile ? '45px' : '50px')};
+  margin: 5px;
 
   &:hover {
-    box-shadow: 0 10px 20px -10px ${(props: { $isLight: boolean }) => (props.$isLight ? colors.GRAY : colors.BORDER_COLOR)};
     cursor: pointer;
   }
 `
