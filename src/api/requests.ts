2b593b5b77b211e0738e5ae9752b9d494dcb0a35
H1K5ik <nikikra2323@gmail.com>
2024-09-05 10:59:09 +0300

    fix: bug with process

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
-require('dotenv').config()
 
 export const getGuthubAccount = async (username: string) => {
-  const { data } = await axios.get(`${process.env.API}${username}`)
+  const { data } = await axios.get(`${process.env.REACT_APP_API}${username}`)
   return data.avatar_url
 }
